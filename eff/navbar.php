140ab8b7ce56975879709513bc2c5b480b03ff15
Simanta Paul <simanta.paul.26@gmail.com>
2019-06-29 14:02:32 +0600

    Edited Navbar of EFF

--- eff/navbar.php
+++ eff/navbar.php
@@ -1,14 +1,17 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">ESIF</a>
+      <a class="navbar-brand" href="index.php">EFF</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php">ESIF</a></li>
+      <li class="active"><a href="index.php">EFF</a></li>
       <li><a href="#">Student List</a></li>
-      <li><a href="#">All Form</a></li>
-      <li><a href="#">ID Card</a></li>
-      <li><a href="#">Ex-Student</a></li>
+      <li><a href="#">Admit Card</a></li>
+      <li><a href="#">Marks Input</a></li>
+      <li><a href="#">Marks Sheet</a></li>
+      <li><a href="#">Tabulation</a></li>
+      <li><a href="#">Transcript</a></li>
+      <li><a href="#">Merit List</a></li>
       <li><a href="#">Search</a></li>
       <li><a href="#">Settings</a></li>
       <li><a href="#">Logout</a></li>
--- index.php
+++ index.php
@@ -3,3 +3,6 @@ session_start();
 if(!$_SESSION['role']){
     header("location:login");
 }
+$category=$_SESSION['category'];
+if($category=="eff") header("location:eff");
+if($category=="esif") header("location:esif");

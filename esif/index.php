378826df8a739ed57229728a3d420932a86ef3f3
Simanta Paul <simanta.paul.26@gmail.com>
2019-07-03 11:28:46 +0600

    Started ESIF Entry Form

--- esif/index.php
+++ esif/index.php
@@ -8,13 +8,34 @@ require_once '../connection/connect.php';
 <!DOCTYPE HTML>
 <html>
     <head>
-       <?php include "../bootstrap/bootstrap.html"; ?>
-        
+        <?php include "../bootstrap/bootstrap.html"; ?>
+
     </head>
     <body>
-      <?php include "navbar.php"; ?>
-      <div class="container">
-      <h1>This is the ESIF Homepage</h1>
-       </div> 
+        <?php include "navbar.php"; ?>
+        <div class="container">
+            <form action="" method="post">
+                <label>Student Full Name</label>
+                <input name="full_name" class="form-control">
+                <?php
+                $result=mysqli_query($db,"SELECT COUNT(*) AS total FROM student");
+                $row=mysqli_fetch_array($result);
+                $student_id=$row['total']+1;
+                $student_id=(string)$student_id;
+                $digit=strlen($student_id);
+                $student_id=sprintf("%05d", $student_id);
+                $student_id="S".$student_id;
+                ?>
+                <label>Student ID</label>
+                <input name="student_id" class="form-control" value="<?php echo $student_id;?>" disabled>
+                <label>Section</label>
+                <select name="section" class="form-control">
+                    <option value="">--SELECT SECTION--</option>
+                    <option value="Boys">Boys</option>
+                    <option value="Girls">Girls</option>
+                    <option value="Combined">Combined</option>
+                </select>
+            </form>
+        </div> 
     </body>
 </html>

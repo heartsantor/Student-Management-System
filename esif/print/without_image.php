33df266ce6caf2f26bf6bfa3e7a345cf9a9e0a5b
Simanta Paul <simanta.paul.26@gmail.com>
2019-09-11 03:55:53 +0600

    DOMpdf

--- esif/print/without_image.php
+++ esif/print/without_image.php
@@ -1,21 +1,35 @@
 <?php
 session_start();
-require_once "../../dompdf/autoload.inc.php";
+//require_once "../../dompdf/autoload.inc.php";
 require_once "../../connection/connect.php";
 //include "partials/bootstrap.html";
 
 
 //Reference the Dompdf namespace
-use Dompdf\Dompdf;
+//use Dompdf\Dompdf;
 
 //Instantiate dompdf class
-$dompdf = new Dompdf();
+//$dompdf = new Dompdf();
 
 //Load HTML content
 //$dompdf->loadHtml("<h1>Welcome to CodexWorld.com</h1>");
 
 //Load content from HTML file
 //$html = file_get_contents("pdf-content.php");
+
+include("../../mpdf/mpdf.php");
+//$mpdf=new mPDF('','A4');
+$mpdf = new mPDF('',    // mode - default ''
+ 'A4',    // format - A4, for example, default ''
+ 0,     // font size - default 0
+ '',    // default font family
+ 15,    // margin_left
+ 15,    // margin right
+ 16,     // margin top
+ 16,    // margin bottom
+ 9,     // margin header
+ 9,     // margin footer
+ 'L'); 
 
 
 $shift=$_GET['shift'];
@@ -38,7 +52,8 @@ $html .= "<html><head>
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
 <style>
 body {
-  font-family: Calibri;
+  font-family: Times New Roman;
+  font-size: 10px;
 }
 th {
   background-color: #b2beb5;
@@ -49,6 +64,7 @@ table, th, td {
   border-collapse: collapse;
   padding: 2px;
   width: 100%;
+  font-size: 10px;
 }
 @page { margin: 0.5in ;}
 </style>
@@ -58,7 +74,7 @@ $html .= "<body>";
 
 
 $html .= "<center>";
-$html .= "<h3><b>".$ins_row['name']."</b></h3>";
+$html .= "<h3 style='font-family:nikosh'><b>".$ins_row['name']."</b></h3>";
 $html .= $ins_row['address']."<br>";
 $html .= "Estd: <b>".date_format(date_create($ins_row['estd']),'m/d/Y')."</b>, EIIN: <b>".$ins_row['eiin']."</b>, Phone: <b>".$ins_row['phone']."</b><br>";
 $html .= "Email: <b>".$ins_row['email']."</b>, Web: <b>".$ins_row['web']."</b><br>";
@@ -95,15 +111,19 @@ while($row=mysqli_fetch_array($result))
 }
 $html .= "</table><body><html>";
 
-$dompdf->loadHtml($html);
+//$dompdf->loadHtml($html);
 
 //Setup paper size
-$dompdf->setPaper('A4', 'landscape');
+//$dompdf->setPaper('A4', 'landscape');
 
 //Render the HTML as PDF
-$dompdf->render();
+//$dompdf->render();
 
 //Output the generated PDF
-$dompdf->stream("Student_list", array("Attachment" => 0));
+//$dompdf->stream("Student_list", array("Attachment" => 0));
+
+
+$mpdf->WriteHTML("$html");
+$mpdf->Output('');
 
 ?>
